15b7d507189296742286085e8ae7af70acb8c7c2
Sarah Takenaga de Abreu <sasasancei@hotmail.com>
2018-07-19 12:00:41 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function Calculadora(){
+var numero1 = parseInt(document.getElementById("numero1").value);
+var numero2 = parseInt(document.getElementById("numero2").value);
+var resultado = document.getElementById("resultado");
+
+
+
+
+switch(Calculadora){
+  case '+' =
+  resultado( (x, y) => {  return x + y; } )( numero1,numero2);break;
+  case '-' =
+  resultado( (x, y) => {  return x - y; } )( numero1,numero2);break;
+  case '/' =
+  resultado( (x, y) => {  return x / y; } )( numero1,numero2);break;
+  case '*' =
+  resultado( (x, y) => {  return x * y; } )( numero1,numero2);break;
+}
+}
